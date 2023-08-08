2390e3740041e6bf96452598e9bd195a78b009af
Lucky Raja <108863687+abcrahulk@users.noreply.github.com>
2023-08-08 15:39:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,72 +9,42 @@ API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 OWNER_ID = int(getenv("OWNER_ID", "5918548652"))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "+oyABz4Lcymw4M2M1")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "+LHcxarl1geQyYWM1")
 UPDATE_CHNL = getenv("UPDATE_CHNL", "ZiddiXBot")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "itz_Lucky_Raja")
 
 # Random Start Images
 IMG = [
-    "https://te.legra.ph/file/04182aca22e7860b732bc.jpg",
-    "https://te.legra.ph/file/1abb4ac383a19ad6c1a64.jpg",
-    "https://te.legra.ph/file/496162fdee5dd6355fd86.jpg",
-    "https://te.legra.ph/file/10df5e259c04ac030e1a6.jpg",
-    "https://te.legra.ph/file/68faec78f390ff0ec1962.jpg",
-    "https://te.legra.ph/file/2d6bc6008eaa2eec77d92.jpg",
-    "https://te.legra.ph/file/a9d63d2a23aa7db7f5f8d.jpg",
-    "https://te.legra.ph/file/7b9232e7ba8e2965bf3df.jpg",
-    "https://te.legra.ph/file/df3f3d39d672d59b81936.jpg",
-    "https://telegra.ph//file/3b59b15e1914b4fa18b71.jpg",
-    "https://te.legra.ph/file/9aba005898bdea06dd949.jpg",
-    "https://telegra.ph//file/ab4925a050e07b00f63c5.jpg",
-    "https://telegra.ph//file/d169a77fd52b46e421414.jpg",
-    "https://telegra.ph//file/dab9fc41f214f9cded1bb.jpg",
-    "https://telegra.ph//file/e05d6e4faff7497c5ae56.jpg",
-    "https://telegra.ph//file/1e54f0fff666dd53da66f.jpg",
-    "https://telegra.ph//file/18e98c60b253d4d926f5f.jpg",
-    "https://telegra.ph//file/b1f7d9702f8ea590b2e0c.jpg",
-    "https://telegra.ph//file/7bb62c8a0f399f6ee1f33.jpg",
-    "https://telegra.ph//file/dd00c759805082830b6b6.jpg",
-    "https://telegra.ph//file/3b996e3241cf93d102adc.jpg",
-    "https://telegra.ph//file/610cc4522c7d0f69e1eb8.jpg",
-    "https://telegra.ph//file/bc97b1e9bbe6d6db36984.jpg",
-    "https://telegra.ph//file/2ddf3521636d4b17df6dd.jpg",
-    "https://telegra.ph//file/72e4414f618111ea90a57.jpg",
-    "https://telegra.ph//file/a958417dcd966d341bfe2.jpg",
-    "https://telegra.ph//file/0afd9c2f70c6328a1e53a.jpg",
-    "https://telegra.ph//file/82ff887aad046c3bcc9a3.jpg",
-    "https://telegra.ph//file/8ba64d5506c23acb67ff4.jpg",
-    "https://telegra.ph//file/8ba64d5506c23acb67ff4.jpg",
-    "https://telegra.ph//file/a7cba6e78bb63e1b4aefb.jpg",
-    "https://telegra.ph//file/f8ba75bdbb9931cbc8229.jpg",
-    "https://telegra.ph//file/07bb5f805178ec24871d3.jpg",
-    "https://telegra.ph/file/ec0ed654f5f5cefc90f95.jpg",
-    "https://telegra.ph/file/f6aa2a3659462401cb600.jpg",
-    "https://telegra.ph/file/0c3d91bcf75524a844883.jpg",
-    "https://telegra.ph/file/6c5df27e71e074f1c7123.jpg",
-    "https://telegra.ph/file/ff2ddc282fe7868e3cf73.jpg",
-    "https://telegra.ph/file/6130ea9373d5f60898a52.jpg",
-    "https://telegra.ph/file/45e5da1eab8f5892981ca.jpg",
+    "https://te.legra.ph/file/759aef893a96bb28034a1.jpg",
+    "https://te.legra.ph/file/8c1be9a35d7e08c0b8e17.jpg",
+    "https://te.legra.ph/file/6701f5fe7e0b8b6e93538.jpg",
+    "https://te.legra.ph/file/459c1a50966643c589551.jpg",
+    "https://te.legra.ph/file/9171909b4618c9baeb0b3.jpg",
+    "https://te.legra.ph/file/0b510f558d4920d900da7.jpg",
+    "https://te.legra.ph/file/759aef893a96bb28034a1.jpg",
 ]
 
 
 # Random Stickers
 STICKER = [
+    "CAACAgUAAxkBAAIJYWTSE7azZor_HMJMWzUMAAFZhBtH5AACTggAAj3xeFYlnsFUsMqQ6zAE",
     "CAACAgUAAxkBAAIJNWTSBk-mL4YYgEkCKysQ3RkZu791AAKQBQACJU7BVaBprfb-3w_9MAQ",
     "CAACAgUAAxkBAAIJKWTSBfsf52WRpHV9bjGmHylENCQ0AAJWBgACnAzBVR4U4AMzmDwLMAQ",
     "CAACAgUAAxkBAAIJMGTSBig-1KPa4RqbehBCdPms7K2JAAJ1BQACyr3BVc5Cv4I_iOLoMAQ",
+    "CAACAgUAAxkBAAIJWWTSEzLjJiuDRwAB1JMgr3h-P9CyHgACKgcAAmoU-FVWxAj4LM5PyjAE",
+    "CAACAgUAAxkBAAIJZmTSE-G52KB93bY-G7AbNigBvghQAAJxBgACu7bQVqEoqjVnpC-BMAQ",
 ]
 
 
 EMOJIOS = [
-    "💣",
-    "💥",
-    "🪄",
-    "🧨",
-    "⚡",
-    "🤡",
-    "👻",
-    "🎃",
-    "🎩",
-    "🕊",
+    "🎉",
+    "😂",
+    "🤗",
+    "😘",
+    "💗",
+    "🥰",
+    "☺️",
+    "😭",
+    "🤩",
+    "🥳",
 ]
